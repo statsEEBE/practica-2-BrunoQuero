724c198e7fad535323654cd6afee15f6cf28d3db
bruno.quero <bruno.quero@auleseebe.upc.edu>
2024-09-26 11:34:18 +0200

    problema 1

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,5 @@
 #Codigo para problema 1
-
+# la r es la sqrt de el multiple rsquared
+# la m es el assets a la altura de estimate
+#la b es el intercept en altura estimate
+#la y=0.06576*x+11.79755 en el punto x= 647.1 = 54-35085
