a51a27d95c0d1578336ff82978edd1d9f712fb07
bruno.quero <bruno.quero@auleseebe.upc.edu>
2024-09-26 11:32:19 +0200

    problema 1

---
+++ Problem1.R
@@ -0,0 +1,6 @@
+## prolema 1
+
+# la r es la sqrt de el multiple rsquared
+# la m es el assets a la altura de estimate
+#la b es el intercept en altura estimate
+#la y=0.06576*x+11.79755 en el punto x= 647.1 = 54-35085
